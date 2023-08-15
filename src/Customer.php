5d5aa10c09f7ccccf869d3779eaaf85fd3288a1b
spacetoescape <spacetoescape@gmail.com>
2023-08-15 20:27:13 +0700

    example of simple composer library

---
+++ src/Customer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SpaceToEscape\Learn;
+
+class Customer
+{
+  public function __construct(private string $name)
+  {
+  }
+
+  public function sayHello(string $name): string
+  {
+    return "Hello " . $name . ", my name is " . $this->name . PHP_EOL;
+  }
+}
